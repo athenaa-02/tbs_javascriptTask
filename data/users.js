eefaba25163823c5ed06f2b64f7261e115aba1e9
natia <natia.karamnishvili369@law.tsu.ge>
2025-07-16 13:09:19 +0400

    working on book borrow and return functions

--- data/users.js
+++ data/users.js
@@ -1,18 +1,21 @@
 const users = [
   {
     userId: 1,
+    name: 'natia',
     currentlyBorrowed: [ ],
     overDue: [],
     totalPenalty: 0,
   },
   {
     userId: 2,
+    name: 'ahmad',
     currentlyBorrowed: [],
     overDue: [],
     totalPenalty: 2,
   },
   {
     userId: 3,
+    name: 'nika',
     currentlyBorrowed: [],
     overDue: [],
     totalPenalty: 5,
--- main.js
+++ main.js
@@ -1,21 +1,51 @@
 function isAvaliable(bookName) {
   const isBorrowed = users.some((user) => {
-   return user.currentlyBorrowed.some((book) => book.name === bookName);
+    return user.currentlyBorrowed.some((book) => book.name === bookName);
   });
-  console.log(isBorrowed)
-  return isBorrowed
-  
+  console.log(isBorrowed);
+  return isBorrowed;
 }
 
+//date function
+function borrowDate() {
+  let date = new Date();
+  const day = date.getDate();
+  const month = date.getMonth() + 1;
+  const year = date.getFullYear();
+  borrowDay = `${day}/${month}/${year}`;
+  return borrowDay;
+}
+
+function returnDate(borrow) {
+  const arr = borrow.split("/").map((part) => Number(part));
+  let day = arr[0];
+  let month = arr[1];
+  let year = arr[2];
+
+  day += 14;
+
+  if (day > 30) {
+    day -= 30;
+    month += 1;
+  }
+  if (month > 12) {
+    month = 1;
+    year += 1;
+  }
+  return `${day}/${month}/${year}`;
+}
+
+//add book function
 function addBook(book) {
-  let lastBookId = Library[Library.length-1].id;
+  let lastBookId = Library[Library.length - 1].id;
   lastBookId++;
 
   book.id = lastBookId;
-  Library.push({...book, avaliable: true});
-  console.log(Library)
+  Library.push({ ...book, avaliable: true });
+//   console.log(Library);
 }
 
+//borrow book function
 function borrowBook(userId, bookName) {
   const activeUser = users.find((u) => u.userId === userId);
   const chosenBook = Library.find((b) => b.name === bookName);
@@ -28,16 +58,30 @@ function borrowBook(userId, bookName) {
     console.log("book not found");
     return;
   }
-  if (chosenBook.avaliable === true) {
+  if (chosenBook.avaliable) {
+    let borrowD = borrowDate();
     console.log("aris", chosenBook.avaliable);
-    let borrowDate = new Date();
-    console.log(borrowDate);
+    console.log("borrow date is:", borrowD);
+    console.log("user must return book:", returnDate(borrowD));
+
     chosenBook.avaliable = false;
   } else {
     console.log("book is unavaliable");
   }
 }
+const borrowingDate = borrowDate()
 
+
+// book returning function
+function returnBook(userName, bookId, borrowDay){
+const activeUser = users.find(user => user.name === userName)
+const booK = Library.find(b => b.id === bookId)
+
+console.log(borrowingDate)
+}
+
+
+//function calls
 addBook({
   name: "Pride and Prejudice",
   author: "Jane Austen",
@@ -45,4 +89,7 @@ addBook({
   rating: 10,
   year: 1813,
 });
-// borrowBook(2, "Pride and Prejudice");
+borrowBook(2, "Pride and Prejudice");
+// borrowBook(3, 'Pride and Prejudice')
+
+returnBook('natia', 2, borrowingDate)
